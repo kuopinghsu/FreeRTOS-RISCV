9c4d960a6fae4f1323004b2fe84aa33518608e96
Kuoping Hsu <kuoping.hsu@gmail.com>
2021-11-08 21:56:37 +0800

    update syscall.c

--- Demo/Common/syscall.c
+++ Demo/Common/syscall.c
@@ -1,22 +1,24 @@
-#include <stdio.h>
 #include <machine/syscall.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/unistd.h>
 #include <errno.h>
-#include "printf.h"
 
 #define HAVE_SYSCALL        0
 #define MEMIO_PUTC          0x9000001c
+#define MEMIO_GETC          0x90000020
 #define MEMIO_EXIT          0x9000002c
 
 // system call defined in the file /usr/include/asm-generic/unistd.h
 enum {
-    SYS_CLOSE   = 0x39,
-    SYS_WRITE   = 0x40,
-    SYS_FSTAT   = 0x50,
-    SYS_EXIT    = 0x5d,
-    SYS_SBRK    = 0xd6
+    SYS_OPEN    = 0xbeef0031,
+    SYS_LSEEK   = 0xbeef0032,
+    SYS_CLOSE   = 0xbeef0039,
+    SYS_READ    = 0xbeef003f,
+    SYS_WRITE   = 0xbeef0040,
+    SYS_FSTAT   = 0xbeef0050,
+    SYS_EXIT    = 0xbeef005d,
+    SYS_SBRK    = 0xbeef00d6
 };
 
 extern int errno;
@@ -43,8 +45,27 @@ __internal_syscall(long n, long _a0, long _a1, long _a2, long _a3, long _a4, lon
     return a0;
 }
 
-void _putchar(char ch) {
+int _putchar(char ch) {
     *(volatile char*)MEMIO_PUTC = (char)ch;
+    return 0;
+}
+
+int _getchar(void) {
+    //TODO
+    //return *(volatile char*)MEMIO_GETC;
+    return -1;
+}
+
+/* open file */
+ssize_t
+_open(const char *pathname, int flags, int mode)
+{
+#if HAVE_SYSCALL
+    int res = __internal_syscall(SYS_OPEN, (long)pathname, (long)flags, (long)mode, 0, 0, 0);
+    return res;
+#else
+    return -1;
+#endif
 }
 
 /* Write to a file.  */
@@ -52,8 +73,8 @@ ssize_t
 _write(int file, const void *ptr, size_t len)
 {
 #if HAVE_SYSCALL
-    __internal_syscall(SYS_WRITE, (long)file, (long)ptr, (long)len, 0, 0, 0);
-    return len;
+    int res = __internal_syscall(SYS_WRITE, (long)file, (long)ptr, (long)len, 0, 0, 0);
+    return res;
 #else
     const char *buf = (char*)ptr;
     int i;
@@ -71,20 +92,42 @@ int _fstat(int file, struct stat *st)
 
 int _close(int file)
 {
+#if HAVE_SYSCALL
+    int res = __internal_syscall(SYS_CLOSE, (long)file, 0, 0, 0, 0, 0);
+    return res;
+#else
     //errno = ENOENT;
     return -1;
+#endif
 }
 
 int _lseek(int file, int ptr, int dir)
 {
+#if HAVE_SYSCALL
+    int res = __internal_syscall(SYS_LSEEK, (long)file, (long)ptr, (long)dir, 0, 0, 0);
+    return res;
+#else
     //errno = ENOENT;
     return -1;
+#endif
 }
 
+#include <stdio.h>
 int _read(int file, char *ptr, int len)
 {
-    //errno = ENOENT;
-    return -1;
+#if HAVE_SYSCALL
+    int res = __internal_syscall(SYS_READ, (long)file, (long)ptr, (long)len, 0, 0, 0);
+    return res;
+#else
+    char *buf = (char*)ptr;
+    char c = 0;
+    int i = 0;
+    do {
+        c = _getchar();
+        buf[i] = c;
+    } while(++i<len && c != '\n');
+    return len;
+#endif
 }
 
 void _exit(int code) {
